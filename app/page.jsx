08f4d9efb8ce9979123d8b859c1dc43dcf10d896
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-08-29 18:13:53 +0200

    structure done

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text">
+            Discover & Share 
+            <br  className="max-md:hidden"/>
+            <span className="orange_gradient">AI-Powered Prompts</span>
+
+        </h1>
+        <p className="desc text-center">
+            This is a an open-source AI Prompting tool for modern world discovery, creation and sharing creative prompts.
+        </p>
+        {/* feed */}
+    </section>
+  )
+}
+
+export default Home
